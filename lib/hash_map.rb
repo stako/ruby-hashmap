ab2493cecfae7cb1b9a6ca3517b9b98d7e8e58e6
stako <stako@users.noreply.github.com>
2024-02-21 14:30:36 -0800

    make some methods private

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -10,15 +10,6 @@ class HashMap
     @buckets = Array.new(@capacity)
   end
 
-  def hash(key)
-    hash_code = 0
-    prime_number = 31
-
-    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
-
-    hash_code
-  end
-
   def set(key, value)
     @buckets[get_index(key)] = Node.new(key, value)
   end
@@ -35,6 +26,17 @@ class HashMap
 
   def length
     @buckets.count { |element| !element.nil? }
+  end
+
+  private
+
+  def hash(key)
+    hash_code = 0
+    prime_number = 31
+
+    key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
+
+    hash_code
   end
 
   def get_index(key)
