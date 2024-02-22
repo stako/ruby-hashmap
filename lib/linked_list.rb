86ee9ac592fe63304910a172c271e9d7b2ef6166
stako <stako@users.noreply.github.com>
2024-02-21 16:39:15 -0800

    add LinkedList#get_node method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,13 +4,25 @@ Node = Struct.new('Node', :key, :value, :next_node)
 
 # Linked List class
 class LinkedList
-  attr_reader :head
-
   def append(key, value)
     return @head = Node.new(key, value) if @head.nil?
 
     tmp = @head
     tmp = tmp.next_node until tmp.next_node.nil?
     tmp.next_node = Node.new(key, value)
+  end
+
+  def get_node(key)
+    return nil if @head.nil?
+
+    node = @head
+    loop do
+      return node if node.key == key
+
+      node = node.next_node
+      break if node.next_node.nil?
+    end
+
+    nil
   end
 end
